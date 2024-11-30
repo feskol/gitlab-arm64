da7dee8b4c70736aeb114b0cd828f8bc4e93af49
Festim Kolgeci <67876946+feskol@users.noreply.github.com>
2024-11-30 21:32:50 +0100

    debugging

--- tests/unit/workflows/syncversion/extract_new_build_tags_test.sh
+++ tests/unit/workflows/syncversion/extract_new_build_tags_test.sh
@@ -32,20 +32,15 @@ function tear_down() {
 }
 
 function test_file_creation() {
-    export SAVED_CE_LAST_MODIFIED_DATE="2024-10-31T00:00:00.000000Z"
-    export SAVED_EE_LAST_MODIFIED_DATE="2024-10-30T00:00:00.000000Z"
 
-    runScript
+    SAVED_CE_LAST_MODIFIED_DATE="2024-10-31T00:00:00.000000Z" SAVED_EE_LAST_MODIFIED_DATE="2024-10-30T00:00:00.000000Z" runScript
 
     assert_file_exists "new_ce_versions.json"
     assert_file_exists "new_ee_versions.json"
 }
 
 function test_found_new_build_tags() {
-    export SAVED_CE_LAST_MODIFIED_DATE="2024-10-31T00:00:00.000000Z"
-    export SAVED_EE_LAST_MODIFIED_DATE="2024-10-30T00:00:00.000000Z"
-
-    runScript
+    SAVED_CE_LAST_MODIFIED_DATE="2024-10-31T00:00:00.000000Z" SAVED_EE_LAST_MODIFIED_DATE="2024-10-30T00:00:00.000000Z" runScript
 
     assert_not_empty "$(cat "new_ce_versions.json")"
     assert_not_empty "$(cat "new_ee_versions.json")"
@@ -55,10 +50,7 @@ function test_found_new_build_tags() {
 }
 
 function test_no_new_builds() {
-    export SAVED_CE_LAST_MODIFIED_DATE="2030-00-00T00:00:00.000000Z"
-    export SAVED_EE_LAST_MODIFIED_DATE="2030-00-00T00:00:00.000000Z"
-
-    runScript
+    SAVED_CE_LAST_MODIFIED_DATE="2030-00-00T00:00:00.000000Z" SAVED_EE_LAST_MODIFIED_DATE="2030-00-00T00:00:00.000000Z" runScript
 
     assert_same "[]" "$(cat new_ce_versions.json)"
     assert_same "[]" "$(cat new_ee_versions.json)"
