edd1f21f2fc063d0927278ebd97bbffbb5eafb1b
Festim Kolgeci <67876946+feskol@users.noreply.github.com>
2024-12-03 22:19:47 +0100

    Workflow testing on different branches (#6)
    * added feature issue-template
    
    * added license
    
    * refactored workflows & added tests for workflows

--- tests/unit/workflows/syncversion/get_last_update_date_from_file_test.sh
+++ tests/unit/workflows/syncversion/get_last_update_date_from_file_test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#
+#----------------------------------------------------------
 # This file is part of the gitlab-arm64 project.
 #
-# (c) Festim Kolgeci <festim.kolgeci@pm.me>
+# (c) Festim Kolgeci <festim.kolgei@pm.me>
 #
+# For complete copyright and license details, please refer
+# to the LICENSE file distributed with this source code.
+#----------------------------------------------------------
 
 # Import test-case
 source ./helper/workflows/syncversion/test-case.sh
