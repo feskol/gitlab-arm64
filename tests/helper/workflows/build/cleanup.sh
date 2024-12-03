a89b848618830eb5793ed6c0a15bb1103014d551
Festim Kolgeci <67876946+feskol@users.noreply.github.com>
2024-12-03 20:38:29 +0100

    added license

--- tests/helper/workflows/build/cleanup.sh
+++ tests/helper/workflows/build/cleanup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#
+#----------------------------------------------------------
 # This file is part of the gitlab-arm64 project.
 #
-# (c) Festim Kolgeci <festim.kolgeci@pm.me>
+# (c) Festim Kolgeci <festim.kolgei@pm.me>
 #
+# For complete copyright and license details, please refer
+# to the LICENSE file distributed with this source code.
+#----------------------------------------------------------
 
 # Cleanup directories
 rm -rf build
